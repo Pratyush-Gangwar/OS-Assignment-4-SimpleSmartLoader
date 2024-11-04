964da2206d925661e9d54bd5397ec5bc072caaab
Pratyush Gangwar <pratyush23395@iiitd.ac.in>
2024-11-04 22:49:11 +0530

    Added mmap to load segments

--- signals/signal_handler.c
+++ signals/signal_handler.c
@@ -1,5 +1,7 @@
 #include "../loader/loader.h"
 
+#define PAGE_SIZE 4096
+
 int num_pages_allocated = 0;
 int num_page_faults = 0;
 int fragmentation = 0;
@@ -7,6 +9,7 @@ int fragmentation = 0;
 // from loader.c
 extern Elf32_Ehdr *ehdr;
 extern Elf32_Phdr *phdr;
+extern int fd;
 
 int get_segment_idx(void* fault_addr) {
     for(int i = 0; i < ehdr->e_phnum; i++) {    
@@ -19,7 +22,12 @@ int get_segment_idx(void* fault_addr) {
   } 
 
   return -1;
+}
 
+void* round_down(void* fault_addr) {
+    int int_addr = (int) fault_addr;
+    int rounded_down_int_addr = (int_addr/PAGE_SIZE) * PAGE_SIZE; // division operator performs floor-division
+    return (void*) rounded_down_int_addr;
 }
 
 void handler(int sig, siginfo_t* info, void* ucontext) {
@@ -32,6 +40,27 @@ void handler(int sig, siginfo_t* info, void* ucontext) {
         return; // don't allocate page
     }
 
+    void* page_start = round_down(fault_addr); // closest page boundary
+    void* mmap_addr = mmap(page_start, PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+
+    if (mmap_addr == MAP_FAILED) {
+        perror("mmap error");
+        exit(1);
+    }
+
+    if (lseek(fd, phdr[idx].p_offset, SEEK_SET) == -1) { // seek to beginning of segment in file
+        perror("Error while seeking to the executable segment");
+        exit(1);
+    }
+
+    // file may have less than PAGE_SIZE bytes but read() wont give an error
+    int bytes = read(fd, mmap_addr, PAGE_SIZE);
+    if (bytes == -1) { // read segment bytes from file to memory
+        perror("Error while writing bytes from file to virtual memory");
+        exit(1);
+    }
+
+    fragmentation += PAGE_SIZE - bytes;
     num_pages_allocated++;
 }
 
